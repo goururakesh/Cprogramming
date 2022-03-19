b1a1127458b7f40948b738c8e3be5e3dbc2f8d82
goururakesh <rakeshgouru4@gmail.com>
2022-03-19 21:50:00 +0530

    five

---
+++ 5.biodata.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	char name[20],clgname[40],section[10],rollnumber[15],age[5],phonenumber[20],gender[5];
+	printf("Enter your name\n");
+	scanf("%s",&name);
+	printf("Enter your clgname\n");
+	scanf("%s",&clgname);
+	printf("Enter your section \n");
+	scanf("%s",&section);
+	printf("Enter your rollnumber \n");
+	scanf("%s",&rollnumber);
+	printf("Enter your age\n");
+	scanf("%s",&age);
+	printf("Enter your phonenumber\n");
+	scanf("%s",&phonenumber);
+	printf("Enter your gender\n");
+	scanf("%s",&gender);
+}
