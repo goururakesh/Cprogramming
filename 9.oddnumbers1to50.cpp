dab63ed289c68ded2a99ff70315019ceeea792f5
goururakesh <rakeshgouru4@gmail.com>
2022-03-22 19:19:32 +0530

    oddnumbers

---
+++ 9.oddnumbers1to50.cpp
@@ -0,0 +1,12 @@
+//write a program to print 1 to 50 odd numbers
+#include<stdio.h>
+int main()
+{
+	int i;
+	printf("odd numbers between 0 to 50\n");
+	for(i=1;i<=50;i=i+2)
+	{
+		printf("%d\n",i);
+	}
+	printf("\n program  completed successfully");
+}
