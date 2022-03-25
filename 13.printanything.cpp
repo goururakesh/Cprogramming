fb298e138c5db8f45b79a6a8909cbcb1856ae8ea
goururakesh <rakeshgouru4@gmail.com>
2022-03-25 19:19:23 +0530

    13

---
+++ 13.printanything.cpp
@@ -0,0 +1,12 @@
+//write a program to print your input (ask user to type anything and print )
+#include<stdio.h>
+int main()
+{
+	int i;
+	printf("Enter any number\n");
+	scanf("%d",&i);
+	printf("your number is =%d",i);
+	
+	return 0;
+	
+}
