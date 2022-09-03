92115b68c4d433889eec3f8e6ea771df25bb311e
goururakesh <rakeshgouru4@gmail.com>
2022-09-03 23:22:37 +0530

    r1

---
+++ 77_dimulti.r1.c
@@ -0,0 +1,12 @@
+//7  wap to print multiplication of 2 numbers in di ?
+#include<stdio.h>
+int main()
+{
+	int a,b,multi;
+	printf("Enter any two numbers=");
+	scanf("%d%d",&a,&b);
+	multi=a*b;
+	printf("%d",multi);
+	
+	return 0;
+}
