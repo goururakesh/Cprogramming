e1e663fc576a60d7c83b2ee01fb76b86a2a5f5d3
goururakesh <rakeshgouru4@gmail.com>
2022-09-03 23:04:37 +0530

    r1

---
+++ 55_disum.r1.c
@@ -0,0 +1,12 @@
+//5  wap to print sum of 2 numbers in dynamic intilization?
+#include<stdio.h>
+int main()
+{
+	int a,b,sum;
+	printf("Enter any two numbers=");
+	scanf("%d%d",&a,&b);
+	sum=a+b;
+	printf("%d",sum);
+	
+	return 0;
+}
