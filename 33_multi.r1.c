44b901f34213f49aefd6eca26d17944d43dec0c7
goururakesh <rakeshgouru4@gmail.com>
2022-09-03 22:39:26 +0530

    r1

---
+++ 33_multi.r1.c
@@ -0,0 +1,10 @@
+//3  wap to print multiplication of 2 numbers in si ?
+#include<stdio.h>
+int main()
+{
+	int a=75,b=20,multi;
+	printf("%d %d\n",a,b);
+	multi=a*b;
+	printf("%d",multi);
+	return 0;
+}
