d85784449aced216b890baff039e18f470a14e18
goururakesh <rakeshgouru4@gmail.com>
2022-09-03 23:18:18 +0530

    r1

---
+++ 66_disub.r1.c
@@ -0,0 +1,12 @@
+//6  wap to print suttraction of 2 numbers in di ?
+#include<stdio.h>
+int main()
+{
+	int a,b,sub;
+	printf("Enter any two numbers=");
+	scanf("%d%d",&a,&b);
+	sub=a-b;
+	printf("%d",sub);
+	
+	return 0;
+}
