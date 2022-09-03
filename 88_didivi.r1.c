f8b464965f2f42f97cd5ae91c9a3bdf2f33855a3
goururakesh <rakeshgouru4@gmail.com>
2022-09-03 23:26:45 +0530

    r1

---
+++ 88_didivi.r1.c
@@ -0,0 +1,12 @@
+//8  wap to print division of 2 numbers in di ?
+#include<stdio.h>
+int main()
+{
+	int a,b,divi;
+	printf("Enter any two numbers=");
+	scanf("%d%d",&a,&b);
+	divi=a/b;
+	printf("%d",divi);
+	
+	return 0;
+}
