741b735c67c645e405a8018af32d60ed79e5a051
goururakesh <rakeshgouru4@gmail.com>
2022-09-03 22:06:32 +0530

    r1

---
+++ 11_stastic.r1.c
@@ -0,0 +1,10 @@
+//1  wap to print sum of 2 numbers in static intilization?
+#include<stdio.h>
+int main()
+{
+	int a=25,b=20,sum;
+	printf("%d %d\n",a,b);
+	sum=a+b;
+	printf("%d",sum);
+	return 0;
+}
